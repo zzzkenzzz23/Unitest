140d49ddb9f48f3e6be7fee80768c1c903c7cb90
oNguyenHaPhan <nhoxvip95@gmail.com>
2017-12-08 19:32:06 +0700

    Unitest

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def show
+  	@user = User.find_by id: params[:id]
+  end
+
+  def new
+  	@user = User.new	
+  end
+
+  def create
+  	@user = User.new params[:user]
+  	if @user.save
+  		redirect_to @user
+  		flash[:success] = "Success"
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+end
