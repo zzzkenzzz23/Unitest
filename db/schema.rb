056b3318469e872a65aa5ccbffcd89b7b72ce151
oNguyenHaPhan <nhoxvip95@gmail.com>
2017-12-08 19:51:44 +0700

    rebuild

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20171206072342) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
